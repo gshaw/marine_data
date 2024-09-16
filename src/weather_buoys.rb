52399de35fc3fe878f55d0195d93687705b087de
Gerry Shaw <gerry_shaw@yahoo.com>
2024-09-16 10:18:17 -0700

    Improve weather_buoys parsing

--- src/weather_buoys.rb
+++ src/weather_buoys.rb
@@ -13,10 +13,21 @@ def download(url)
   URI.open(url).read
 end
 
-def strip_html(html)
+def parse_coordinate(location)
+  if location =~ /(\d+\.\d+)\s+([NS])\s+(\d+\.\d+)\s+([EW])/
+    latitude = $2 == "N" ? $1.to_f : -$1.to_f
+    longitude = $4 == "E" ? $3.to_f : -$3.to_f
+    [latitude.round(5), longitude.round(5)]
+  else
+    [nil, nil]
+  end
+end
+
+def parse_note(html)
   escaped = CGI.unescapeHTML(html.to_s.gsub(/<.*?>/, " "))
     .gsub(" \.", "\.")
     .gsub("&nbsp;", " ")
+    .gsub(" ,", ",")
     .gsub("\t", " ")
     .gsub(/\s+/, " ")
   stripped_or_nil(escaped)
@@ -56,6 +67,7 @@ def build_station_owners
 
     code = data[0]
     owners[code] = {
+      code: code,
       name: data[1],
       countryCode: data[2]
     }
@@ -77,40 +89,39 @@ def build_stations(station_ids_with_buoy_cams, owners)
     # 13009|PR|Atlas Buoy|PM-533|Lambada||8.000 N 38.000 W (8&#176;0'0" N 38&#176;0'0" W)|| |
 
     station_id = data[0].upcase
-    has_buoy_cam = station_ids_with_buoy_cams.include?(station_id)
-
-    location = data[6].to_s
-    if location =~ /(\d+\.\d+)\s+([NS])\s+(\d+\.\d+)\s+([EW])/
-      latitude = $2 == "N" ? $1.to_f : -$1.to_f
-      longitude = $4 == "E" ? $3.to_f : -$3.to_f
-    else
-      puts "skipping invalid coordinate: " + line
-      next
-    end
-
-    owner_code = data[1]
-    owner = owners[owner_code]
+    owner = owners[data[1]]
     if owner == nil
       puts "skipping unknown owner: " + line
       next
     end
 
-    forecasts = data[8].to_s.split
+    type = data[2]
+    hull = data[3]
+    name = data[4]
+    payload = data[5]
 
-    note = strip_html(data[9].to_s)
+    latitude, longitude = parse_coordinate(data[6].to_s)
+    if latitude == nil || longitude == nil
+      puts "skipping invalid location: " + line
+      next
+    end
+
+    forecasts = data[8].to_s.split
+    note = parse_note(data[9].to_s)
+    has_buoy_cam = station_ids_with_buoy_cams.include?(station_id)
 
     buoys << {
       id: station_id,
-      latitude: latitude.round(5),
-      longitude: longitude.round(5),
-      name: data[4],
-      ownerCode: owner_code,
+      latitude: latitude,
+      longitude: longitude,
+      name: name,
+      ownerCode: owner[:code],
       ownerName: owner[:name],
       ownerCountryCode: owner[:countryCode],
       hasBuoyCam: has_buoy_cam,
-      type: data[2],
-      hull: data[3],
-      payload: data[5],
+      type: type,
+      hull: hull,
+      payload: payload,
       forecasts: forecasts,
       note: note
     }
